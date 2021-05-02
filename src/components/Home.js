beffe74c94515710304202d79121a5030662b14c
Julio Adrian Peña Romero <j.adrian.1690@gmail.com>
2021-05-02 00:11:10 -0500

    build: getting answer and not answer questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-const Home = () => {
-
+const Home = props => {
+    //console.log(props)
     return (
         <h1>Home</h1>
     )
 }
 
-export default Home
+const mapStateToProps = ({questions, users, authedUser}) => {
+    let orderedQuestionsIds = Object.keys(questions).sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+    let answers = users[authedUser.id].answers
+
+    let answersIds = [];
+    let notAnswersIds = [];
+
+    for (let question of orderedQuestionsIds){
+        answers[question] ?
+            answersIds.push(question)
+            :
+            notAnswersIds.push(question)
+    }
+
+    return {
+        answersIds,
+        notAnswersIds
+    }
+}
+
+export default connect(mapStateToProps)(Home)
